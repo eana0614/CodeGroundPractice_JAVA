107029c6413e64325a1ac701793c3d347a66f927
EANA <EANA@EANA-DESKTOP>
2017-10-10 23:22:01 +0900

    Update Practice Problem1

--- CodeGroundPractice/src/Problem1/Solution.java
+++ CodeGroundPractice/src/Problem1/Solution.java
@@ -1,10 +1,11 @@
 package Problem1;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /*
  * program is receives N numbers.
- * And output is result of XOR operation use only odd numbers among n numbers.
+ * And output is result of XOR operation use only odd(th) appear numbers among n numbers.
  * 
  * All test case Num : <= 20
  * All run time Limit : <= 1s (Java : <=2s)
@@ -16,20 +17,40 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int T = sc.nextInt();
-		
-		for(int test_case = 0; test_case < T; test_case++) {
+		sc.nextLine();
+
+		for (int test_case = 0; test_case < T; test_case++) {
 			
 			int Ansewer = 0;
 			
-			
-			System.out.println("Case #"+(test_case+1));
+			int testElementNum = sc.nextInt();
+			sc.nextLine();
+
+			String test = sc.nextLine();
+			Ansewer = runSoultion(test, testElementNum);
+
+			System.out.println("Case #" + (test_case + 1));
 			System.out.println(Ansewer);
 		}
+
+	}
+
+	private static int runSoultion(String test, int size) {
+
+		StringTokenizer stringTokenizer = new StringTokenizer(test, " ");
 		
+		int result = Integer.parseInt(stringTokenizer.nextToken());
+		while(stringTokenizer.hasMoreTokens()) {
+			
+			result = result ^ Integer.parseInt(stringTokenizer.nextToken());
+
+		}
+
+		return result;
 	}
 
 }
