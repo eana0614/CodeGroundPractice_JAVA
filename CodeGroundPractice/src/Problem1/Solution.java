5876842fbd81541a0d1c2ac62c8e6fdcb5cc44d2
EANA <EANA@EANA-DESKTOP>
2017-10-10 22:38:16 +0900

    Add Problem1 Info

--- CodeGroundPractice/src/Problem1/Solution.java
+++ CodeGroundPractice/src/Problem1/Solution.java
@@ -1,10 +1,35 @@
 package Problem1;
 
+import java.util.Scanner;
+
+/*
+ * program is receives N numbers.
+ * And output is result of XOR operation use only odd numbers among n numbers.
+ * 
+ * All test case Num : <= 20
+ * All run time Limit : <= 1s (Java : <=2s)
+ * 
+ * Output : Case #T (T:test case number)
+ * 			test case answer
+ */
+
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int test_case = 0; test_case < T; test_case++) {
+			
+			int Ansewer = 0;
+			
+			
+			System.out.println("Case #"+(test_case+1));
+			System.out.println(Ansewer);
+		}
+		
 	}
 
 }
